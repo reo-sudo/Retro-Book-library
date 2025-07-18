00c4b80673a0e2e608846ca83bb59d3d64c4db65
reo-sudo <ruderask15@gmail.com>
2025-07-18 14:01:37 +1200

    Update models.py
    fixs

--- Retro webapp/myproject/RetroApp/models.py
+++ Retro webapp/myproject/RetroApp/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Book(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     authors = models.CharField(max_length=255, blank=True)
     description = models.TextField(blank=True)
@@ -20,4 +19,5 @@ class LibraryEntry(models.Model):
     def __str__(self):
         return f"{self.user.username}' copy of {self.book.title}"
     
+
 
